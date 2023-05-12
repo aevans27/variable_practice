4fcff5d9703ec325151e31b131dd3a5d098014a6
Allan Evans <oneblankpage27@gmail.com>
2023-05-12 14:01:00 -0600

    Added Puts statements

--- variables.rb
+++ variables.rb
@@ -6,3 +6,9 @@ var_wife_loves = "Dogs"
 var_int = 12345
 var_float = 12.34
 var_bool = false
+var_name.replace('Ajay')
+var_dog.replace('Dog')
+var_dog_name.replace('Pudge')
+puts "My dogs name is #{var_dog_name}"
+puts "I get called #{var_name}"
+puts "My wifes names is #{var_wife}"
