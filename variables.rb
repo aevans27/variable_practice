c973b7bdde9b9419f8b5b2abd583c4e9d2a075e9
Allan Evans <oneblankpage27@gmail.com>
2023-05-15 17:48:23 -0600

    Update value replacement and variable names

--- variables.rb
+++ variables.rb
@@ -1,14 +1,14 @@
-var_name = "Allan"
-var_dog = "Husky"
-var_dog_name = "Apollo"
-var_wife = "Alison"
-var_wife_loves = "Dogs"
-var_int = 12345
-var_float = 12.34
-var_bool = false
-var_name.replace('Ajay')
-var_dog.replace('Dog')
-var_dog_name.replace('Pudge')
-puts "My dogs name is #{var_dog_name}"
-puts "I get called #{var_name}"
-puts "My wifes names is #{var_wife}"
+first_name = "Allan"
+dog_type = "Husky"
+dog_name = "Apollo"
+wifes_name = "Alison"
+wife_loves = "Dogs"
+months_in_year = 12
+change_from_lunch = 12.34
+is_dog_good = false
+first_name = "Ajay"
+dog_type = "Dog"
+dog_name = "Pudge"
+puts "My dogs name is #{dog_name}"
+puts "I get called #{first_name}"
+puts "My wifes names is #{wifes_name}"
